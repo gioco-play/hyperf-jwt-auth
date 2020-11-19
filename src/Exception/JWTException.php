7b54a41639ec7cc371f7af15e4033e157746880b
Xuan Yang <xuan.yang@goldenf.co>
2020-11-19 12:49:25 +0800

    first commit

---
+++ src/Exception/JWTException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liyuzhao
+ * Date: 2019-08-01
+ * Time: 13:43
+ */
+
+namespace GiocoPlus\JWTAuth\Exception;
+
+use PDO;
+
+class JWTException extends \RuntimeException
+{
+    
+}
---
+++ src/JWTInterface.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: liyuzhao
+ * Date: 2020/4/21
+ * Time: 1:34 下午
+ */
+
+namespace GiocoPlus\JWTAuth;
+
+/**
+ * Interface JWTInterface
+ * @package GiocoPlus\JWTAuth
+ */
+interface JWTInterface
+{
+    public function setSceneConfig(string $scene = 'default', $value = null);
+    public function getSceneConfig(string $scene = 'default');
+    public function setScene(string $scene);
+    public function getScene();
+}
